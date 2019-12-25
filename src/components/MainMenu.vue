e491bc667a4e6b7591571b8858238af310fe96cc
unknown <brognolijoao@hotmail.com>
2019-12-25 00:08:03 -0300

    feat(menu): introduce main menu

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row class="mt-8">
+    <v-col cols="12">
+      <BaseButton @click="handlePlay">Jogar</BaseButton>
+    </v-col>
+    <v-col cols="12">
+      <BaseButton>Regras</BaseButton>
+    </v-col>
+    <v-col cols="12">
+      <BaseButton>Sobre</BaseButton>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  name: 'MainMenu',
+  components: {
+    BaseButton: () => import('@/components/BaseButton'),
+  },
+  data: () => ({}),
+  methods: {
+    ...mapMutations('user', ['updateChoosePlayers']),
+    handlePlay() {
+      this.updateChoosePlayers(true)
+    },
+    handleRules() {},
+    handleAbout() {},
+  },
+}
+</script>
