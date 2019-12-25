f3784b1f4a0494c7ed62b090570f4f603605a1e0
unknown <brognolijoao@hotmail.com>
2019-12-24 22:06:32 -0300

    feat(base): introduce button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn dark outlined block :style="styles" v-bind="$attrs" v-on="$listeners">
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  inheritAttrs: false,
+  props: {
+    square: Boolean,
+  },
+  computed: {
+    styles() {
+      return {
+        borderRadius: this.square ? '2px' : undefined,
+      }
+    },
+  },
+}
+</script>
